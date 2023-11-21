9f482f1122462a361e972f6bc824a7010e152e86
jrgonzalez <jgonzalez@datamex.com.py>
2023-11-21 11:31:27 -0300

    ok telegram bot

---
+++ include/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '683d4f10bf8c30c22e78fdce4334e6278ce1f499',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '683d4f10bf8c30c22e78fdce4334e6278ce1f499',
+            'dev_requirement' => false,
+        ),
+        'telegram-bot/api' => array(
+            'pretty_version' => 'v2.5.0',
+            'version' => '2.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../telegram-bot/api',
+            'aliases' => array(),
+            'reference' => 'eaae3526223db49a1bad76a2dfa501dc287979cf',
+            'dev_requirement' => false,
+        ),
+    ),
+);
