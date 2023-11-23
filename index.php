2b9b15cd893c0adc685e5faee3cc51ef69475b4e
jrgonzalez3 <jrgonzalez3@gmail.com>
2023-11-23 08:31:40 -0300

    ok

--- index.php
+++ index.php
@@ -3,17 +3,66 @@ use TelegramBot\Api\BotApi;
 
 require_once 'include/vendor/autoload.php';
 
-//TODO token de acceso 
-$botToken = '6543217813:AAHVEbzoOWBmniWlu1vrQcgwk0_az3ElUgs';
-$webhookUrl = 'https://jringenieriayservicios.com/bot/index.php';
+loadEnv();
+function loadEnv()
+{
+    $envFilePath = __DIR__ . '/.env';
+    // Check if the .env file exists
+    if (!file_exists($envFilePath)) {
+        die('.env file not found. Please create one.');
+    }
 
+    // Read .env file contents
+    $envFileContents = file_get_contents($envFilePath);
+    $lines = explode("\n", $envFileContents);
+
+    foreach ($lines as $line) {
+        // Ignore comments and empty lines
+        $line = trim($line);
+        if (empty($line) || $line[0] === '#') {
+            continue;
+        }
+
+        // Parse the line and set environment variables
+        list($key, $value) = explode('=', $line, 2);
+
+        // Set environment variable
+        if (putenv("$key=$value")) {
+            // Define constant
+            $constantName = strtoupper(str_replace(' ', '_', $key));
+            defined($constantName) or define($constantName, getenv($key));
+
+            // echo "</br> $constantName --- Configured...\n";
+        } else {
+            echo "Error configuring $key...\n";
+        }
+    }
+}
+
+$botToken = TGBOTTOKEN;
+$webhookUrl = WEBHOOKURL;
 $telegram = new BotApi($botToken);
-
 $update = json_decode(file_get_contents('php://input'));
+
 
 if (isset($update->message->text)) {
     $chatId = $update->message->chat->id;
     $message = $update->message->text;
+
+    // Expresión regular para limpiar y validar el comando "/insertar"
+    $pattern = "/\/$mensaje\s+([0-9a-zA-Z]+)/";
+    $matches = [];
+
+    if (preg_match($pattern, $message, $matches)) {
+        // La expresión regular coincide, $matches[1] contiene el valor limpio
+        $cleanedValue = strtoupper(trim($matches[1]));
+
+        // Ahora $cleanedValue contiene el valor convertido a mayúsculas y sin espacios al principio o al final
+        echo "Valor limpio: $cleanedValue";
+    } else {
+        // El comando no coincide con el formato esperado
+        echo "Comando no válido";
+    }
 
     switch ($message) {
         case '/start':
@@ -24,13 +73,12 @@ if (isset($update->message->text)) {
             break;
         case '/menu':
             $menu = "/menu - Este es el menu principal\n";
-            //   $menu = "/menu - Para mostrar el menu principal\n";
+            $menu .= "/Consultaruc - Para mostrar consultar el Ruc\n";
             $telegram->sendMessage($chatId, $menu);
             break;
-        case '/Hola':
-            $menu = "HOla Dr\n";
-            //   $menu = "/menu - Para mostrar el menu principal\n";
-            $telegram->sendMessage($chatId, $menu);
+        case '/Consultaruc':
+            $data = consultarWS($nrodocumento);
+            $telegram->sendMessage($chatId, $data);
             break;
 
         default:
@@ -41,4 +89,68 @@ if (isset($update->message->text)) {
 
     }
 
+
+
+
+
 }
+
+
+
+function consultarWS($nrodocumento)
+{
+    if ($nrodocumento > 0) {
+        $url = URLAPIFS . '/ruc/' . $nrodocumento;
+        $resp = httpPost($url, APIKEYFS);
+        echo trim($resp);
+    } else {
+        echo json_encode(
+            array(
+                "Status" => false,
+                "textStatus" => "Error Consulta",
+            )
+        );
+    }
+}
+
+function httpPost($url, $apikey)
+{
+    $curl = curl_init();
+    if (ENVIRONMENT != 'production') {
+        $ssl = false;
+    } else {
+
+        $ssl = true;
+    }
+
+    curl_setopt_array(
+        $curl,
+        array(
+            CURLOPT_URL => "$url",
+            CURLOPT_SSL_VERIFYPEER => $ssl,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 60,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_VERBOSE => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Bearer api_key_" . $apikey,
+            ),
+        )
+    );
+    $response = curl_exec($curl);
+    if ($response === false) {
+        $error = curl_error($curl);
+        $errorCode = curl_errno($curl);
+        echo "CURL Error: $error (Error Code: $errorCode)";
+    }
+    curl_close($curl);
+    return $response;
+}
+
+
+
+
